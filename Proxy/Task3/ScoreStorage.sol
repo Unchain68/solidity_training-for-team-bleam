1a03d679ca44fded304c7a573de90e43e4587300
Jae Gyeoung Yeom <109026317+Unchain68@users.noreply.github.com>
2022-12-12 17:21:28 +0900

    Update ScoreStorage.sol

--- Proxy/Task3/ScoreStorage.sol
+++ Proxy/Task3/ScoreStorage.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract ScoreStorage {
+
+  mapping(bytes32=>uint) uints;
+  
+  function setUints(bytes32 key, uint score) public {
+    uints[key] = score;
+  }
+  
+  function getUints(bytes32 key) public view returns(uint) {
+    return uints[key];
+  }
+}
