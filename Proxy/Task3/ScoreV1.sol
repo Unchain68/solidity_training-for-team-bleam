f5f4d4dc235e6efb1c4a08e9e47e479c1bb59a1d
Jae Gyeoung Yeom <109026317+Unchain68@users.noreply.github.com>
2022-12-12 17:25:04 +0900

    Update ScoreV1.sol

--- Proxy/Task3/ScoreV1.sol
+++ Proxy/Task3/ScoreV1.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
 
+import "./ScoreStorage.sol";
+
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract ScoreV1 {
+
+  ScoreStorage ss; 
+  
+  bytes32 public constant SCORE = keccak256("score");
+  
+  constructor(address scoreStorage) {
+    ss = ScoreStorage(scoreStorage);
+  }
+  
+  function setScore(uint _score) public {
+    ss.setUints(SCORE,_score);
+  }
+}
