9f85757c5ef062b39150ca652a417523b87d42cc
Jae Gyeoung Yeom <109026317+Unchain68@users.noreply.github.com>
2022-12-13 17:37:45 +0900

    Update ScoreV1.sol

--- Proxy/Task4/ScoreV1.sol
+++ Proxy/Task4/ScoreV1.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+import "./ProxyStorage.sol";
+import "./ScoreStorage.sol";
+
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract ScoreV1 is ProxyStorage, ScoreStorage {
+
+    function setScore(uint256 _score) {
+        score = _score;
+    }
+    
+    function getEncodedSignature(string memory _func, uint256 num) 
+        external 
+        pure 
+        returns(bytes memory) 
+    {
+        return abi.encodedWithSignature(_func, num);
+    }
+}
 
